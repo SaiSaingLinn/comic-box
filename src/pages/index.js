f47c0836488f07e1cfc63a1bdd461dbf07746e5d
Sai Saing Linn <saisainglinn.me@gmail.com>
2022-05-20 14:43:50 +0630

    fixed home api call

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,21 @@
 import Home from 'src/pages/home'
 
 export default function Index(props) {
-  console.log('props', props)
   return (
     <Home />
   )
 }
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
 
-  const res = await fetch(`${process.env.NEXT_API_BASE_URL}/books/list?perPage=8&page=1`)
-  const posts = await res.json()
-  // const category = await JSON.parse(JSON.stringify(data));
+//   const res = await fetch(`${process.env.NEXT_API_BASE_URL}/books/list?perPage=8&page=1`)
+//   const posts = await res.json()
+//   // const category = await JSON.parse(JSON.stringify(data));
 
-  return {
-    props: { 
-      posts,
-    },
-    revalidate: 10,
-  };
-}
+//   return {
+//     props: { 
+//       posts,
+//     },
+//     revalidate: 10,
+//   };
+// }
